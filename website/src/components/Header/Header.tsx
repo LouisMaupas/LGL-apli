dad28972b5092f74457ee4aa52d4ec9a5c4f99bf
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-21 11:55:50 +0200

    feat(header): add header with nav links

---
+++ website/src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../../AuthProvider";
+import "./header.css";
+import logo from "../../assets/logo.png";
+
+/**
+ * Renders the Header component
+ *
+ * @return {JSX.Element} The rendered Header component
+ */
+const Header = (): JSX.Element => {
+  const auth = useAuth();
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              <img className="logo" src={logo} alt="Home" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/download">Téléchargement</Link>
+          </li>
+          <li>
+            <Link to="/profil">Profil</Link>
+          </li>
+          {auth?.user ? (
+            <li>
+              <button
+                onClick={() => {
+                  auth?.signout(() => {});
+                }}
+              >
+                Se deconnecter
+              </button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Se connecter</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
