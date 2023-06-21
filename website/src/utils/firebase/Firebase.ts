09bc0df73c89968821401cf5a33f7595111bb308
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-21 16:15:22 +0200

    fix: use vercel var env

--- website/src/utils/firebase/Firebase.ts
+++ website/src/utils/firebase/Firebase.ts
@@ -6,9 +6,9 @@ import { initializeApp } from "firebase/app";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || process.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ||process.env.VITE_FIREBASE_PROJECT_ID,
   };
 
 // Initialiser Firebase
