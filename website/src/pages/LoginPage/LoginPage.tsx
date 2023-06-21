0425a8cef2ce0e22f07c18886591d54a5d0fdcb1
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-21 12:03:05 +0200

    feat: login feature

---
+++ website/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,78 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAppInstance } from "../../utils/firebase/Firebase";
+import { useEffect } from "react";
+import { useAuth } from "../../AuthProvider";
+
+/**
+ * Renders a login page that allows users to sign in with their credentials. If the login is successful,
+ * the user is redirected back to the page they tried to visit before they were redirected to the login page.
+ *
+ * @param {React.FormEvent<HTMLFormElement>} event - The form submit event.
+ * @return {JSX.Element} The login page component.
+ */
+const LoginPage = () => {
+  const auth = useAuth(),
+    navigate = useNavigate(),
+    location = useLocation(),
+    [apiError, setApiError] = useState("");
+  let authInstance,
+    from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    getAppInstance()
+      .then((app) => {
+        // Initialize Firebase Authentication and get a reference to the service
+        authInstance = getAuth(app);
+      })
+      .catch((error) => {
+        console.error("Failed to initialize Firebase app:", error);
+      });
+  }, []);
+
+  /**
+   * Handles the login form submission
+   *
+   * @param {React.FormEvent<HTMLFormElement>} event - The form submit event.
+   */
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { username, password } = event.currentTarget;
+    signInWithEmailAndPassword(authInstance, username.value, password.value)
+      .then((userCredential) => {
+        auth?.signin(userCredential.user, () => {
+          // Send them back to the page they tried to visit when they were
+          // redirected to the login page.
+          navigate(from, { replace: true });
+        });
+      })
+      .catch((error) => {
+        setApiError(
+          `Erreur lors de la connexion ${error.code} ${error.message}`
+        );
+      });
+  };
+
+  return (
+    <div>
+      {from !== "/" ? (
+        <div>Vous devez vous connectez pour accèder à la page {from}</div>
+      ) : null}
+
+      <form onSubmit={handleLogin}>
+        <label>
+          Username: <input name="username" type="text" />
+        </label>{" "}
+        <label>
+          Mot de passe: <input name="password" type="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+
+      <p>{apiError}</p>
+    </div>
+  );
+};
+
+export default LoginPage;
