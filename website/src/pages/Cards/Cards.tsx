f42acbfd3675471152464e1a5f9bd279f3b7806a
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-21 16:52:06 +0200

    add file

---
+++ website/src/pages/Cards/Cards.tsx
@@ -0,0 +1,10 @@
+const Cards = () => {
+  return (
+    <>
+      <h1>Bibliothèque des cartes</h1>
+      <p>Prochainement : la liste de toutes les cartes.</p>
+    </>
+  );
+};
+
+export default Cards;
