71bfd52f1316d01b955090e65ab9c00b624dc154
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-21 11:57:51 +0200

    feat: add firebase config

---
+++ website/src/utils/firebase/Firebase.ts
@@ -0,0 +1,39 @@
+// firebase
+import { Firestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  };
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+const db : Firestore = getFirestore(app);
+  
+/**
+ * Returns the Firebase instance asynchronously.
+ *
+ * @return {Promise<App>} A promise resolving to the app instance.
+ */
+async function getAppInstance() {
+  return app;
+}
+
+  // Get a list of cities from database to check if the API is working
+  async function getCities(db: Firestore) {
+    const citiesCol = collection(db, 'cities');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+    return cityList;
+  }
+
+  // Test if the API is working
+  // getCities(db).then((cities) => {
+  //   console.log("api res fonctionne bien = ", cities);
+  // })
+
+export {getAppInstance}
