42f6a1bf885d8f75dc04c5fb6d66aaf634640e72
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-06-23 12:44:00 +0200

    feat: wip i18n

--- website/src/pages/HowToPlay/HowToPlay.tsx
+++ website/src/pages/HowToPlay/HowToPlay.tsx
@@ -1,89 +1,67 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-
 import iconValue from "../../assets/icon_value.png";
 import iconCost from "../../assets/icon_cost.png";
 import iconPower from "../../assets/icon_power.png";
 
 const HowToPlay = () => {
+  const { t } = useTranslation();
   return (
     <>
       <div>
         <p>
-          Duel of Reigns est un jeu de cartes stratégique se déroulant en deux
-          manches. Chaque manche est composée de 12 duels, où deux joueurs
-          s'affrontent en posant des cartes. Le but est de remporter le plus de
-          points possible pour devenir le gagnant de la partie. Chaque joueur
-          constitue un <a href="#deck">deck</a> de 24 cartes, composé de 6
-          cartes de 4 <a href="#faction">factions</a> différentes. Chaque carte
-          a une{" "}
+          {t("howToPlay.description.term1")}
+          {t("howToPlay.description.term2")}
+          <a href="#deck">{t("howToPlay.description.term3")}</a>
+          {t("howToPlay.description.term4")}
+          <a href="#faction">{t("howToPlay.description.term5")}</a>
+          {t("howToPlay.description.term6")}
           <a href="#power">
-            puissance
+            {t("howToPlay.description.term7")}
             <img height={30} src={iconPower}></img>
           </a>
-          et une{" "}
+          {t("howToPlay.description.term8")}
           <a href="#value">
-            valeur
+            {t("howToPlay.description.term9")}
             <img height={30} src={iconValue}></img>
           </a>
-          , normale ou boostée lorsqu'elle est jouée pendant son règne. Les
-          cartes appartiennent à différentes factions telles que les humains,
-          les elfes, les orcs et les démons. Lors d'un duel, les joueurs posent
-          une carte à tour de rôle. La carte avec la puissance la plus élevée
-          remporte le pli, et les valeurs des deux cartes sont ajoutées au score
-          du gagnant. Les duels se déroulent en suivant un déroulement précis,
-          avec des phases d'activation, de pose, de combat et de score. Les
-          joueurs disposent de
+          {t("howToPlay.description.term10")}
           <a href="#po">
-            pièces d'or (Po)
+            {t("howToPlay.description.term11")}
             <img height={30} src={iconCost}></img>
           </a>
-          qu'ils peuvent dépenser pour inclure des cartes dans leur deck. Chaque
-          joueur commence avec 240 Po. Le coût des cartes doit être géré pour
-          construire un deck équilibré et efficace. Le jeu propose également des
-          éléments tels que des effets spéciaux et des cas particuliers qui
-          peuvent influencer le déroulement des duels et des manches. À la fin
-          des deux manches, le joueur ayant obtenu le score le plus élevé
-          remporte la partie. C'est un jeu stratégique où la composition du
-          deck, la gestion des ressources et la prise de décision tactique sont
-          essentielles pour atteindre la victoire.
+          {t("howToPlay.description.term12")}
         </p>
-        <h3>Règles</h3>
-        <h4>Général</h4>
-        <p>Termes :</p>
+        <h3>{t("howToPlay.rules")}</h3>
+        <h4>{t("howToPlay.general")}</h4>
+        <p>{t("howToPlay.Rules")}</p>
         <ul>
-          <li>Partie = l'ensemble des 2 manches.</li>
-          <li>Manche = 12 duels.</li>
-          <li>Duel : voir déroulement d'un duel.</li>
+          <li>{t("howToPlay.game")}</li>
+          <li>{t("howToPlay.round")}</li>
+          <li>{t("howToPlay.duel")}</li>
+          <li>{t("howToPlay.power.desc")}</li>
           <li>
-            Puissance = la valeur qui détermine quelle carte gagne le duel.
+            <span id="value">{t("howToPlay.value")}</span>
+            {t("howToPlay.valueDesc")}
           </li>
+          <li>{t("howToPlay.cost")}</li>
           <li>
-            <span id="value">Valeur</span> = nombre de points qui seront ajoutés
-            au score du gagnant du duel.
+            <span id="po">{t("howToPlay.goldCoin")}</span>
+            {t("howToPlay.goldCoin.desc")}
           </li>
-          <li>
-            Coût : le nombre de pièces d'or à dépenser pour inclure la carte à
-            son deck.
-          </li>
-          <li>
-            <span id="po">Pièce d'or (Po)</span> : monnaie permettant d'acheter
-            des cartes pour les inclure dans son deck. Chaque joueur possède 240
-            Po.
-          </li>
-          <li>Effet : voir le google forms excel.</li>
+          <li>{t("howToPlay.effect")}</li>
         </ul>
 
         <h3>
-          <span id="faction">Factions</span>
+          <span id="faction">{t("howToPlay.factions")}</span>
         </h3>
-        <p>Il existe plusieurs factions :</p>
+        <p>{t("howToPlay.factionsDescription")}</p>
 
-        <h3>Cartes</h3>
-        <Link to={"/cards"}>Voir toutes les cartes</Link>
-        <p>Les informations concernant les cartes :</p>
+        <h3>{t("howToPlay.cards")}</h3>
+        <Link to={"/cards"}>{t("howToPlay.cards.link")}</Link>
+        <p>{t("howToPlay.cards.info")}</p>
         <ul>
           <li>
             <span id="power">Une puissance</span> normale / une puissance
